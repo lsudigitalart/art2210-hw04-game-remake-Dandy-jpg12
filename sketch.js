a1fb430f77af3a6de250060ef7750f41d8ee4fd3
Skylar <sconle3@lsu.edu>
2025-09-28 23:14:46 -0500

    finish Update

--- sketch.js
+++ sketch.js
@@ -2,284 +2,226 @@
 let creepyImage;
 let creepyAlpha = 0;
 
-let gameDuration = 15;
-let gameStartTime;
-let SpawningEnabled = true;
-
 let playerX = 200;
 let playerY = 300;
 let playerSize = 50;
 let speed = 15;
 let enemySize = 60
 
-let enemyX = 90
-let enemyY= 10
-let enemySpeed = 5
-let enemySpeedX = 7
+let pinkX = 300
+let pinkY = 200
+let PinkSize = 20
 
-let enemybY = 600
-let enemybX = 0
-let enemybSpeed = 3.5
-let enemybSpeedX = -5
+let orangeX = 300
+let orangeY = 200
+let orangeSize = 30
 
-let enemycY = -600
-let enemycX = 0
-let enemycSpeed = -4.5
-let enemycSpeedX = -5
+let Enemy1X = 0
+let Enemy1Y = 0
+let Enemy1Speed = 25
+let Enemy1Size = 50
 
-let enemydY = -600
-let enemydX = 0
-let enemydSpeed = -5.5
-let enemydSpeedX = 5
+let Enemy2X = 0
+let Enemy2Y = 0
+let Enemy2Speed = 15
+let Enemy2Size = 50
 
-let bigboyX;
-let bigboyY;
-let bigboySize = 800
-let bigboySpeed = 3
-let bigboyActive = false
-let bigboyCollided = false
+let score = 0
+let gametime = 0
+let timelimit = 1800
+
+let enemyVisibility = true;
+let enemyVisibility2 = true;
 
 
+let blackorbX = 0 - 700
+let blackorbY = 260
+let blackorbSize = 700
+let blackorbSpeed = 3
+
+let blackorbSPAWN = false;
+let Orbtime = 0
+let SPAWNDELAY = 16000
+
+let enemyRespawn = 0
+let enemyRespawn2 = 0
 
 let isGameOver = false;
+
+//Evil image
+let Spooky
+let SpookyAlpha = 0
+function preload() {Spooky = loadImage("for game.png");
+
+}
 
 var whichscreen = "start"
 
 function setup() {
   createCanvas(800, 600);
 
-  InitializeEnemies();
-
-  gameStartTime = millis()  
-
-}
-
-function preload() {
-    creepyImage = loadImage("for game.png");
+    Orbtime = millis() + SPAWNDELAY;
 
   }
 
+
+  
 function draw(){
-    if (isGameOver) {
-        endScreen();
-        return;
-    }
-
-
-if (whichscreen === "start") {
-    startScreen();
-    } else if (whichscreen === "maingame"){
-    mainGame();
-    } else if (whichscreen === "end"){
-    endScreen();
-    } else if (whichscreen === "Creepy") {
-    creepyEndScreen();
-    }
-}
-
+   
+    background(10, 70, 150)
     
-function startScreen() {
-    background("blue");
-    textSize(40);
-    textAlign(CENTER, CENTER);
-    fill(255);
-    text("Press Any Button to Start", width / 2, height / 1.5);
-    text("Avoid the Circles!", width / 2, height / 2.5);
-
-
-
-    if (keyIsPressed) {
-        whichscreen = "maingame";
-        gameStartTime = millis();
-        isGameOver = false;
-        SpawningEnabled = true;
-        bigboyActive = false;
-        bigboyX = undefined;
-        InitializeEnemies();
-
-    }
-}
-
-
-function mainGame() {
     
-    background(200);
-
-let timePassed = (millis() - gameStartTime) / 1000;
-
-if (timePassed >= gameDuration) {
-    SpawningEnabled = false;
-    bigboyActive = true;
-    }   
-
-
-
-    if (bigboyActive && bigboyX === undefined){
-        bigboyX = -bigboySize;
-        bigboyY = height / 2;
-    }
-
-    if (keyIsDown(LEFT_ARROW)) 
+    
+    if (keyIsDown(LEFT_ARROW) && playerX > playerSize/2)
         {playerX -= speed;
         }
 
-    if (keyIsDown(RIGHT_ARROW)) 
+    if (keyIsDown(RIGHT_ARROW) && playerX < width - playerSize/2) 
         {playerX += speed;
         }
 
-    if (keyIsDown(UP_ARROW)) 
+    if (keyIsDown(UP_ARROW) && playerY > playerSize/2) 
         {playerY -= speed; 
         }
 
-    if (keyIsDown(DOWN_ARROW)) {
+    if (keyIsDown(DOWN_ARROW) && playerY < height - playerSize/2) {
         playerY += speed;
     }
 
+    // Player Constraints
     playerX = constrain(playerX, playerSize/2, width - playerSize/2);
     playerY = constrain(playerY, playerSize/2, height - playerSize/2);
 
-    if (enemyY > height + enemySize) { enemyY = -enemySize;
-    enemyX = random(enemySize, width / 2 - enemySize);
-
+    let distance = dist(playerX, playerY, pinkX, pinkY);
+    if (distance < (playerSize + PinkSize) / 2) {score += 50;
+ 
+    // Collectibles :D
+        pinkX = random(PinkSize, width - PinkSize);
+    pinkY = random(PinkSize, height - PinkSize);
     }
     
-     fill(255, 200, 100);
-        circle(playerX, playerY, playerSize);
-    
-    if (SpawningEnabled) {
+    let distance2 = dist(playerX, playerY, orangeX, orangeY);
+    if (distance2 < (playerSize + orangeSize) / 2) {score += 20
 
-     fill(155, 20, 10);
-        
-
-        circle(enemydX, enemydY, enemySize);
-
-        circle(enemycX, enemycY, enemySize);
-        
-        circle(enemybX, enemybY, enemySize);
-        
-        circle(enemyX, enemyY, enemySize);
+        orangeX = random(orangeSize, width - orangeSize);
+        orangeY = random(orangeSize, height - orangeSize);
     }
 
-    if (bigboyActive) {
-        fill("black");
-        circle(bigboyX, bigboyY, bigboySize);
-        bigboyX += bigboySpeed;
+    // Enemy
+    if (enemyVisibility) {
+    Enemy1X += Enemy1Speed;
 
-        let bigboyDistance = dist(playerX, playerY, bigboyX, bigboyY);
-        if (bigboyDistance < (playerSize + bigboySize) / 2) {
-            bigboyCollided = true;
-            whichscreen = "Creepy";
-            return;
+    fill("black")
+    square(Enemy1X, Enemy1Y, Enemy1Size);
+    
+    if (Enemy1X > width + Enemy1Size) {
+        enemyVisibility = false;
+        enemyRespawn = millis() + 2000
+    }
+    
+
+    let distance3 = dist(playerX, playerY, Enemy1X + Enemy1Size/ 2, Enemy1Y + enemySize/2);
+    if (enemyVisibility && distance3 < (playerSize + enemySize)/2) {score -= 20;
+        enemyVisibility = false;
+    }
+    } else {
+        if (millis() >= enemyRespawn) {
+        Enemy1X = -Enemy1Size;
+        Enemy1Y = random(Enemy1Size, height - Enemy1Size);
+        enemyVisibility = true;
         }
-    }
-
-    enemyY += enemySpeed
-    enemyX += enemySpeedX
-    enemybY += enemybSpeed
-    enemybX += enemybSpeedX
-    enemycY += enemycSpeed
-    enemycX += enemycSpeedX
-    enemydY += enemydSpeed
-    enemydX += enemydSpeedX
-
-
-
-    if (enemybY > height + enemySize) { enemybY = -enemySize; enemybX = random
-        (width / 2 + enemySize, width - enemySize)}
-
-     if (enemycY + enemySize < 0 ) { enemycY = height + enemySize; enemycX = random
-        (enemySize, width - enemySize)}   
-
-    if (enemydY < -enemySize) { enemydX > width + enemySize; enemydX = random
-        (enemySize, enemySize * 2); enemydY = height + enemySize}  
-
-    let distance = dist(playerX, playerY, enemyX + enemySize/2, enemyY + enemySize/2);
-    if (distance < (playerSize + enemySize) / 2) {isGameOver = true;
-        whichscreen = "end";
 
     }
-     
-if (SpawningEnabled) {
-    let distance2 = dist(playerX, playerY, enemybX + enemySize / 2, enemybY + enemySize/2);
-    if (distance2 < (playerSize + enemySize) / 2) {isGameOver = true;
-        whichscreen = "end";
-    }
 
-    let distance3 = dist(playerX, playerY, enemycX + enemySize / 2, enemycY + enemySize/2);
-    if (distance3 < (playerSize + enemySize) / 2) {isGameOver = true;
-        whichscreen = "end";
-    }
+    //Enemy 2
+if (enemyVisibility2) {
+    Enemy2X += Enemy2Speed;
 
-    let distance4 = dist(playerX, playerY, enemydX + enemySize / 2, enemydY + enemySize/2);
-    if (distance4 < (playerSize + enemySize) / 2) {isGameOver = true;
-        whichscreen = "end";
-    }
-    }
-}  
-
-
-   function endScreen() {
-    background("pink")
+    fill("black")
+    square(Enemy2X, Enemy2Y, Enemy2Size);
     
-    fill("red")
-    textAlign(CENTER, CENTER);
-    textSize(50)
-    text("Try Again!", width / 2, height / 2 )
+    if (Enemy2X > width + Enemy2Size) {
+        enemyVisibility2 = false;
+        enemyRespawn2 = millis() + 2000
+    }
     
-    textSize(20)
-    text("Click to Restart", width / 2, 400)
+
+    let distance4 = dist(playerX, playerY, Enemy2X + Enemy2Size/ 2, Enemy2Y + Enemy2Size/2);
+    if (enemyVisibility2 && distance4 < (playerSize + Enemy2Size)/2) {score -= 20;
+        enemyVisibility2 = false;
+    }
+    } else {
+        if (millis() >= enemyRespawn2) {
+        Enemy2X = -Enemy2Size;
+        Enemy2Y = random(Enemy2Size, height - Enemy2Size);
+        enemyVisibility2 = true;
+        }
+
+    }
+
+    //black Orb of despair
+
+    if (millis() >= Orbtime && !blackorbSPAWN) {
+        blackorbSPAWN = true
+    }
+    if (blackorbSPAWN){
+        blackorbX += blackorbSpeed; 
+    }
+
+    circle(blackorbX, blackorbY, blackorbSize);
+
+   
+
+    if (blackorbX > width + blackorbSize);
+
+    let distance5 = dist(playerX, playerY, blackorbX + blackorbSize/
+        2, blackorbY + blackorbSize/2);
+
+    if (distance5 < (playerSize + blackorbSize)/2) {
+        isGameOver = true;
+    }
+
+    if (isGameOver) {
+        drawGameOver();
+        return;
    }
 
-     function creepyEndScreen() {
-        
-        background("black");
+    //Shapes
+    fill(200)
+    circle(playerX, playerY, playerSize);
+    
+    fill("pink")
+    circle(pinkX, pinkY, PinkSize)
+
+    fill("orange")
+    circle(orangeX, orangeY, orangeSize)
+
+    fill(255);
+    textSize(20);
+    text("Score: " + score, 20, 30);
+    
+  
+    function drawGameOver() {
+
+        background(0);
         if (creepyAlpha < 255){
             creepyAlpha += 1
         }
         tint(255, creepyAlpha);
-        image(creepyImage, 0 ,0, width, height);
-        
+        image(Spooky, 0, 0, width, height);
 
-        textSize(32)
+        textSize(25);
         fill(255, creepyAlpha);
-        
 
-        for (let i = 0; i < 20; i++){
+        for (let i = 0; i < 30; i++){
             let x = random(width);
-            let y = random(height);
-            fill("red")
-            text("YOU LOSE", x, y);
+            let y = random (height);
+        text("YOU LOSE", x, y)
+        fill("red");
+
+
         }
-     }
-
-function restartGame() {
-    whichscreen = "start"
-    isGameOver = false;
-    SpawningEnabled = true;
-    bigboyActive = false;
-    bigboyX = undefined;
-    creepyAlpha = 0;
-    InitializeEnemies();
-    gameStartTime = millis();
-}
-
-function mousePressed(){
-    if (whichscreen === "end" || whichscreen === "Creepy") {restartGame();
-
+        
     }
+
 }
-
-     function InitializeEnemies() {
-        enemyX = random(enemySize, width / 2 - enemySize);
-    enemyY = random(-300,0);
-
-    enemybX = random(width / 2 + enemySize, width - enemySize);
-    enemybY = random(-300,0);
-
-    enemycX = random(enemySize, width - enemySize);
-    enemycY = (height + enemySize);
-
-    enemydX = random(enemySize, enemySize * 2);
-    enemydY = (height + enemySize);
-     }
-    
